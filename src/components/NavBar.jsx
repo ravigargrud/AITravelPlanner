9d0880ff3bbbbbad10b1b76ab07f4a1c4804e925
Ravi Garg <ravigargrud@gmail.com>
2024-11-25 15:20:36 +0530

    v1 finalised

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
     return (
         <>
-            <nav>
+            <nav className={styles.mainBar}>
                 <Link to="/">
                 <img src={Logo} alt="" />
                 </Link>
